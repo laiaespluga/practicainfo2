1e2c3b06f9360280e5ecc67fc1b669233063c994
Oriol Vilà <101257154+OVyla@users.noreply.github.com>
2023-10-23 09:00:14 +0200

    Funció d'inicialització
    s'ha afegit la funció d'inicialització

--- practica2.c
+++ practica2.c
@@ -1,1 +1,20 @@
-##Oriol Vilà i Laia Espluga
+# Oriol Vilà i Laia Espluga
+void InitData(){
+int i,j;
+srand(8824553);
+for( i = 0; i < N; i++ )
+  for( j = 0; j < N; j++ ){
+  Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+    if ( (abs(i - j) <= 3) && (i != j))
+      MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+    else if ( i == j )
+      MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
+    else MatDD[i][j] = 0.0;
+ }
+  
+for( i = 0; i < N; i++ ){
+  V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+  V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+  V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+}
+}
